f0a15d8a262b49dd15de69a25e1b55452ad9a233
pranavraikote <pranavraikote@gmail.com>
2019-11-03 03:16:40 +0530

    Added new stylesheet

--- student.php
+++ student.php
@@ -6,7 +6,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="icon" href="img/BMSIT_1.ico">
 	<link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-	<link rel="stylesheet" a href="css/style.css">	
+	<link rel="stylesheet" a href="css/studentstyle.css">	
 </head>
 
 <body>
@@ -15,13 +15,12 @@
 		<img src="img/img.png"/>
 		<p id="login_status"></p>
 		<form method="POST" action ="modules/verify_login.php" id="form">
-			<div class="form-input">
+			
 				<input type="text" name="username" placeholder="Enter Username"/>	
-			</div> <br>
-			<div class="form-input">
+            			
 				<input type="password" name="password" placeholder="Enter Password"/>
-			</div> <br>
-				<input type="submit" type="submit" value="LOGIN" class="btn-login"/>
+			
+				<input type="submit" class="btn-login" type="submit" value="SUBMIT" />
 		</form>
 	</div>
 </body>
