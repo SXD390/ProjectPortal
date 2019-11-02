df45717dc83a59e8378d6768d2baafd39ffb694c
pranavraikote <pranavraikote@gmail.com>
2019-11-03 01:23:57 +0530

    Few changes to project admin

--- admin.php
+++ admin.php
@@ -5,9 +5,10 @@
 	<title> Admin </title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="icon" href="img/BMSIT_1.ico">
-	<link rel="stylesheet" a href="css\style.css">
-	<link rel="stylesheet" a href="css\font-awesome.min.css">
+	<link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+	<link rel="stylesheet" a href="css\style.css">	
 </head>
+
 <body>
 	<div class="container">
 		<h1>Welcome to Admin Portal</h1><br><br><br><br>
@@ -16,10 +17,10 @@
 </p>
 		<form method="POST" action ="modules/verify_login.php">
 			<div class="form-input">
-				<input type="text" name="username" placeholder="Enter the User Name"/>	
+				<input type="text" name="username" placeholder="Enter Username"/>	
 			</div> <br>
 			<div class="form-input">
-				<input type="password" name="password" placeholder="Enter password here"/>
+				<input type="password" name="password" placeholder="Enter Password"/>
 			</div> <br>
 				<input type="submit" type="submit" value="LOGIN" class="btn-login"/>
 		</form>
