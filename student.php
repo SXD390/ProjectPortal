6eafb8b7dda7bcf91cc6dd5c0047958fdd20fa79
pranavraikote <pranavraikote@gmail.com>
2019-11-03 03:30:47 +0530

    Added form in Student

--- student.php
+++ student.php
@@ -2,7 +2,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title> Admin </title>
+	<title> Student </title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="icon" href="img/BMSIT_1.ico">
 	<link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -12,13 +12,21 @@
 <body>
 	<div class="container">
 		<h1>Welcome to Student Portal</h1><br><br><br><br>
-		<img src="img/img.png"/>
-		<p id="login_status"></p>
+		<img src="img/img.png"/>		
 		<form method="POST" action ="modules/verify_login.php" id="form">
-			
-				<input type="text" name="username" placeholder="Enter Username"/>	
+			<div class="form-input">
+				<input type="text" name="name" placeholder="Student Name"/>	
             			
-				<input type="password" name="password" placeholder="Enter Password"/>
+                <input type="password" name="usn" placeholder="Student USN"/> <br>
+
+                <input type="text" class = "email" name="email" placeholder="Student Email ID"/> <br>
+                
+                <select name="project" class="list">
+                    <option value="a">Project A</option>
+                    <option value="b">Project B</option>
+                    <option value="c">Project C</option>
+                    <option value="d">Project D</option>
+                </select> <br>
 			
 				<input type="submit" class="btn-login" type="submit" value="SUBMIT" />
 		</form>
