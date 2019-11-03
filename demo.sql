62089eedfde91b99ca013237454b1680721b5c46
SXD390 <sudarshanravi13@gmail.com>
2019-11-03 15:38:13 +0530

    changed sql

--- demo.sql
+++ demo.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 02, 2019 at 05:30 PM
+-- Generation Time: Nov 03, 2019 at 11:07 AM
 -- Server version: 10.4.6-MariaDB
 -- PHP Version: 7.3.8
 
@@ -41,6 +41,70 @@ CREATE TABLE `loginform` (
 INSERT INTO `loginform` (`ID`, `User`, `Pass`) VALUES
 ('101', 'admin', 'admin');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `projects`
+--
+
+CREATE TABLE `projects` (
+  `project_id` int(11) NOT NULL,
+  `project_name` varchar(45) NOT NULL,
+  `subject_code` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `projects`
+--
+
+INSERT INTO `projects` (`project_id`, `project_name`, `subject_code`) VALUES
+(101, '', ''),
+(1001, 'Professional Elective Management System', '15CS71'),
+(1007, 'Entrepreneur Information Mgmt System', '15CS71'),
+(1024, 'Project Portal Management System', '15CS71');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student`
+--
+
+CREATE TABLE `student` (
+  `usn` varchar(10) NOT NULL,
+  `name` varchar(25) NOT NULL,
+  `email` varchar(25) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `student`
+--
+
+INSERT INTO `student` (`usn`, `name`, `email`) VALUES
+('1BY15CS115', 'Harshit Gupta', 'gupta@gmail.com'),
+('1BY16CS006', 'Akshay Padesur', 'akshay@gmail.com'),
+('1BY16CS062', 'Pranav Raikote', 'pranavraikote@gmail.com'),
+('1BY16CS084', 'Sudarshan Venkatesh', 'sudarshanravi13@gmail.com');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `works_on`
+--
+
+CREATE TABLE `works_on` (
+  `usn` varchar(10) NOT NULL,
+  `project_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `works_on`
+--
+
+INSERT INTO `works_on` (`usn`, `project_id`) VALUES
+('1BY16CS084', 1024),
+('1BY16CS062', 1024),
+('1BY15CS115', 1024);
+
 --
 -- Indexes for dumped tables
 --
@@ -51,6 +115,19 @@ INSERT INTO `loginform` (`ID`, `User`, `Pass`) VALUES
 ALTER TABLE `loginform`
   ADD PRIMARY KEY (`ID`),
   ADD UNIQUE KEY `User` (`User`);
+
+--
+-- Indexes for table `projects`
+--
+ALTER TABLE `projects`
+  ADD PRIMARY KEY (`project_id`);
+
+--
+-- Indexes for table `student`
+--
+ALTER TABLE `student`
+  ADD PRIMARY KEY (`usn`),
+  ADD UNIQUE KEY `email` (`email`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
