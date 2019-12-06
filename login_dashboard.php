ef2ea0c9c6c8961a767c8eadd05181e5d699d50c
Sudarshan Venkatesh <39019569+SXD390@users.noreply.github.com>
2019-12-06 11:19:17 +0530

    Updated Files
    Latest Version....

---
+++ login_dashboard.php
@@ -0,0 +1,54 @@
+<?php
+	session_start();
+	session_destroy();
+?>
+<!doctype html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Project Portal</title>
+		<link rel="stylesheet" type="text/css" href="css/a11.css">
+	</head>
+	<body style="background-image: url('img/bmsit_back.jpg'); background-size: cover;">
+		<div class="container">
+			<div class="card">
+				<div class="face face1">
+					<div class="content">
+						<img src="img/img2.png">
+						<h3>ADMIN</h3>	
+					</div>	
+				</div>
+				<div class="face face2">
+					<div class="content">
+						<p>The Admin page gives you access to the Analytics administrative features. Click below Use these tools to create new properties, change user permissions, add integrations,import data.   
+						</p>	
+						<a href='modules/login.php'>Click Here</a>
+					</div>	
+				</div>
+				</div>
+			<div class="card" >
+				<div class="face face1">
+					<div class="content">
+						<img src="img/img1.png">
+						<h3>STUDENT</h3>	
+					</div>	
+				</div>
+				<div class="face face2">
+					<div class="content">
+						<p>The login page allows a user to gain access to an application by entering their username and password or by authenticating using a social media login. 
+						</p>	
+
+						<a href='slogin.php'>Click Here</a> 
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                            
+                        </a>    
+                            </div>
+					</div>	
+			</div>
+			</div>
+		 </div>
+	</body>
+
